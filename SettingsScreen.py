9506ff0ba395b291d6dd68870ca956b9ffb553cb
CRehfuss <crehfuss@umich.edu>
2015-04-18 22:47:15 -0400

    Settings chosen box
    Now it shows what dragon you picked and wow I’m super tired and engr100
    suxs

--- SettingsScreen.py
+++ SettingsScreen.py
@@ -31,16 +31,24 @@ def Settings(dragon_choice,sound_choice):
             
     redDragBtn = Button((90, 170), (115, 110))
     redHighLight = HighlightButton((255,255,255), 'hoverbox.png')
+    redChosen = HighlightButton((255,255,255), 'chosenbox.png')
     redHL = 0
+    redOn = 0
     orangeDragBtn = Button((225, 170), (115, 105))
     orangeHighLight = HighlightButton((255,255,255), 'hoverbox.png')
+    orangeChosen = HighlightButton((255,255,255), 'chosenbox.png')
     orangeHL = 0
+    orangeOn = 0
     bandgDragBtn = Button((370, 170), (120, 105))
     bandgHighLight = HighlightButton((255,255,255), 'hoverbox.png')
+    bandgChosen = HighlightButton((255,255,255), 'chosenbox.png')
     bandgHL = 0
+    bandgOn = 0
     blkDragBtn = Button((510, 170), (120, 105))
     blkHighLight = HighlightButton((255,255,255), 'hoverbox.png')
+    blkChosen = HighlightButton((255,255,255), 'chosenbox.png')
     blkHL = 0
+    blkOn = 0
     
     onBtn = Button((345, 340), (85, 60))
     offBtn = Button((255, 340), (75, 60))
@@ -49,6 +57,27 @@ def Settings(dragon_choice,sound_choice):
     screen.blit(color, [0, 400])
     screen.blit(background, [0,0])
     pygame.display.update()
+
+    if(dragon_choice=="red"):
+        screen.blit(redChosen.image, [80,165]) # RED
+        pygame.display.update()
+        redOn = 1
+    
+    if(dragon_choice=="orange"):
+        screen.blit(orangeChosen.image, [220,165]) # o
+        pygame.display.update()
+        orangeOn = 1
+        
+    if(dragon_choice == "greenandblue"):
+        screen.blit(bandgChosen.image, [365,165]) # bandg
+        pygame.display.update()
+        bandgOn = 1
+        
+    if(dragon_choice == "black"):
+        screen.blit(blkChosen.image, [505,165]) # blk
+        pygame.display.update()
+        blkOn = 1
+        
 
     while state != 1:
         #print "settings page"
@@ -65,15 +94,34 @@ def Settings(dragon_choice,sound_choice):
                 screen.blit(redHighLight.image, [80,165]) # RED
                 pygame.display.update()
                 redHL = 1
-                print "OVER RED"
             if (redHL == 1 and redDragBtn.rect.collidepoint(pygame.mouse.get_pos())==False):
-                print "UNBLIT R"
                 objectsonscreen = [background]
                 screen.blit(background, (0,0))
-                pygame.display.update()
+                if(redOn==1):
+                    screen.blit(redChosen.image, [80,165]) # RED
+                    pygame.display.update()
+                if(orangeOn ==1):
+                    screen.blit(orangeChosen.image, [220,165]) # o
+                    pygame.display.update()
+                if(bandgOn == 1):
+                    screen.blit(bandgChosen.image, [365,165]) # bandg
+                    pygame.display.update()
+                if(blkOn ==1):
+                    screen.blit(blkChosen.image, [505,165]) # blk
+                    pygame.display.update()
                 redHL = 0
             if event.type == MOUSEBUTTONDOWN and redDragBtn.rect.collidepoint(pygame.mouse.get_pos()):
+                if (orangeOn == 1 or bandgOn == 1 or blkOn == 1):
+                    objectsonscreen = [background]
+                    screen.blit(background, (0,0))
+                    orangeOn = 0
+                    bandgOn=0
+                    blkOn=0
+                    
                 dragon_choice = "red"
+                redOn = 1
+                screen.blit(redChosen.image, [80,165]) # RED
+                pygame.display.update()
                 if(sound_choice==1):
                     click_Sound.play()
                     
@@ -83,13 +131,33 @@ def Settings(dragon_choice,sound_choice):
                 pygame.display.update()
                 orangeHL = 1
             if (orangeHL == 1 and orangeDragBtn.rect.collidepoint(pygame.mouse.get_pos())==False):
-                print "UNBLIT O"
                 objectsonscreen = [background]
                 screen.blit(background, (0,0))
-                pygame.display.update()
+                if(redOn==1):
+                    screen.blit(redChosen.image, [80,165]) # RED
+                    pygame.display.update()
+                if(orangeOn ==1):
+                    screen.blit(orangeChosen.image, [220,165]) # o
+                    pygame.display.update()
+                if(bandgOn == 1):
+                    screen.blit(bandgChosen.image, [365,165]) # bandg
+                    pygame.display.update()
+                if(blkOn ==1):
+                    screen.blit(blkChosen.image, [505,165]) # blk
+                    pygame.display.update()
                 orangeHL = 0  
             if event.type == MOUSEBUTTONDOWN and orangeDragBtn.rect.collidepoint(pygame.mouse.get_pos()):
+                if (redOn == 1 or bandgOn == 1 or blkOn == 1):
+                    objectsonscreen = [background]
+                    screen.blit(background, (0,0))
+                    redOn = 0
+                    bandgOn = 0
+                    blkOn = 0
+                    
                 dragon_choice = "orange"
+                screen.blit(orangeChosen.image, [220,165]) # RED
+                pygame.display.update()
+                orangeOn = 1
                 if(sound_choice==1):
                     click_Sound.play()
             
@@ -99,13 +167,32 @@ def Settings(dragon_choice,sound_choice):
                 pygame.display.update()
                 bandgHL = 1
             if (bandgHL == 1 and bandgDragBtn.rect.collidepoint(pygame.mouse.get_pos())==False):
-                print "UNBLIT bandg"
                 objectsonscreen = [background]
                 screen.blit(background, (0,0))
-                pygame.display.update()
+                if(redOn==1):
+                    screen.blit(redChosen.image, [80,165]) # RED
+                    pygame.display.update()
+                if(orangeOn ==1):
+                    screen.blit(orangeChosen.image, [220,165]) # o
+                    pygame.display.update()
+                if(bandgOn == 1):
+                    screen.blit(bandgChosen.image, [365,165]) # bandg
+                    pygame.display.update()
+                if(blkOn ==1):
+                    screen.blit(blkChosen.image, [505,165]) # blk
+                    pygame.display.update()
                 bandgHL = 0    
             if event.type == MOUSEBUTTONDOWN and bandgDragBtn.rect.collidepoint(pygame.mouse.get_pos()):
+                if (orangeOn == 1 or redOn == 1 or blkOn == 1):
+                    objectsonscreen = [background]
+                    screen.blit(background, (0,0))
+                    orangeOn = 0
+                    redOn = 0
+                    blkOn = 0
                 dragon_choice = "greenandblue"
+                screen.blit(bandgChosen.image, [365,165]) # RED
+                pygame.display.update()
+                bandgOn = 1
                 if(sound_choice==1):
                     click_Sound.play()
                     
@@ -115,13 +202,32 @@ def Settings(dragon_choice,sound_choice):
                 pygame.display.update()
                 blkHL = 1
             if (blkHL == 1 and blkDragBtn.rect.collidepoint(pygame.mouse.get_pos())==False):
-                print "UNBLIT BLK"
                 objectsonscreen = [background]
                 screen.blit(background, (0,0))
-                pygame.display.update()
+                if(redOn==1):
+                    screen.blit(redChosen.image, [80,165]) # RED
+                    pygame.display.update()
+                if(orangeOn ==1):
+                    screen.blit(orangeChosen.image, [220,165]) # o
+                    pygame.display.update()
+                if(bandgOn == 1):
+                    screen.blit(bandgChosen.image, [365,165]) # bandg
+                    pygame.display.update()
+                if(blkOn ==1):
+                    screen.blit(blkChosen.image, [505,165]) # blk
+                    pygame.display.update()
                 blkHL = 0
             if event.type == MOUSEBUTTONDOWN and blkDragBtn.rect.collidepoint(pygame.mouse.get_pos()):
+                if (orangeOn == 1 or bandgOn == 1 or redOn == 1):
+                    objectsonscreen = [background]
+                    screen.blit(background, (0,0))
+                    orangeOn = 0
+                    bandgOn= 0
+                    redOn = 0
                 dragon_choice = "black"
+                blkOn = 1
+                screen.blit(blkChosen.image, [505,165]) # RED
+                pygame.display.update()
                 if(sound_choice==1):
                     click_Sound.play()
                     
